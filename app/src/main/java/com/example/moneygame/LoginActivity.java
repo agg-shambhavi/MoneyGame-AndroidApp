4de3029ac789293558a994e4b5aad2bbfa9a960c
Shambhavi Aggarwal <agg.shambhavi@gmail.com>
2021-04-04 11:34:19 +0530

    Add login screen ui

---
+++ app/src/main/java/com/example/moneygame/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.moneygame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+}
