27a737c168cd57aaf50555ec52b82da2991e44c3
Shambhavi Aggarwal <agg.shambhavi@gmail.com>
2021-04-10 09:48:41 +0530

    Show user info in the UI

--- app/src/main/java/com/example/moneygame/UserFragment.java
+++ app/src/main/java/com/example/moneygame/UserFragment.java
@@ -4,18 +4,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.Objects;
+
 public class UserFragment extends Fragment {
+
+    public UserFragment() {
+
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
+        View view = inflater.inflate(R.layout.user_fragment, container, false);
+        TextView firstName = view.findViewById(R.id.first_name_user);
+        TextView lastName = view.findViewById(R.id.last_name_user);
+        TextView userEmail = view.findViewById(R.id.user_email_user);
+        TextView regDate = view.findViewById(R.id.user_date_user);
         UserInfoService dashboardService = new UserInfoService(getActivity());
         dashboardService.getUserInfo(new UserInfoService.VolleyResponseListener() {
             @Override
@@ -25,11 +37,17 @@ public class UserFragment extends Fragment {
 
             @Override
             public void onResponse(UserInfoModel userInfo) {
-                Toast.makeText(getActivity(), userInfo.toString(), Toast.LENGTH_LONG).show();
+                firstName.setText(userInfo.getFirst_name());
+                lastName.setText(userInfo.getLast_name());
+                userEmail.setText(userInfo.getUser_email());
+                regDate.setText(userInfo.getRegistration_date());
+
             }
         });
 
-        return inflater.inflate(R.layout.user_fragment, container, false);
+
+        return view;
+
 
     }
 }
